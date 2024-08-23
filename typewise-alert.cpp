344b75d4d06606c08e59f53b3ef57de84331a00f
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-08-23 18:26:43 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,36 +1,64 @@
-#include "battery-alert-system.h"
+#include "typewise-alert.h"
 #include <iostream>
 
-BatteryAlertSystem::BreachType BatteryAlertSystem::inferBreach(double temperature, double lowerLimit, double upperLimit) {
-    if (temperature < lowerLimit) return BreachType::TOO_LOW;
-    if (temperature > upperLimit) return BreachType::TOO_HIGH;
+// Define the cooling limits for different cooling types
+const TypewiseAlert::CoolingLimits coolingLimits[] = {
+    { TypewiseAlert::CoolingType::PASSIVE_COOLING, 0, 35 },
+    { TypewiseAlert::CoolingType::MED_ACTIVE_COOLING, 0, 40 },
+    { TypewiseAlert::CoolingType::HI_ACTIVE_COOLING, 0, 45 }
+};
+
+// Return the cooling limits for a given cooling type
+const TypewiseAlert::CoolingLimits* TypewiseAlert::getCoolingLimits() {
+    return coolingLimits;
+}
+
+// Infer the breach type based on the temperature value and limits
+TypewiseAlert::BreachType TypewiseAlert::inferBreach(double value, double lowerLimit, double upperLimit) {
+    if (value < lowerLimit) return BreachType::TOO_LOW;
+    if (value > upperLimit) return BreachType::TOO_HIGH;
     return BreachType::NORMAL;
 }
 
-BatteryAlertSystem::BreachType BatteryAlertSystem::classifyTemperatureBreach(CoolingType coolingType, double temperature) {
-    switch (coolingType) {
-        case CoolingType::PASSIVE_COOLING:
-            return inferBreach(temperature, 0, 35);
-        case CoolingType::MED_ACTIVE_COOLING:
-            return inferBreach(temperature, 0, 40);
-        case CoolingType::HI_ACTIVE_COOLING:
-            return inferBreach(temperature, 0, 45);
-        default:
-            return BreachType::NORMAL;
+// Get the cooling limits for a specific cooling type
+TypewiseAlert::CoolingLimits TypewiseAlert::getLimitsForCoolingType(CoolingType coolingType) {
+    const CoolingLimits* limits = getCoolingLimits();
+    for (int i = 0; i < sizeof(coolingLimits) / sizeof(coolingLimits[0]); ++i) {
+        if (limits[i].coolingType == coolingType) {
+            return limits[i];
+        }
+    }
+    // Default limits in case of an unknown cooling type
+    return { coolingType, 0, 0 };
+}
+
+// Classify the temperature breach based on the cooling type and temperature
+TypewiseAlert::BreachType TypewiseAlert::classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
+    CoolingLimits limits = getLimitsForCoolingType(coolingType);
+    return inferBreach(temperatureInC, limits.lowerLimit, limits.upperLimit);
+}
+
+// Send alert to the controller based on the breach type
+void TypewiseAlert::sendToController(BreachType breachType) {
+    std::cout << "feed : " << static_cast<int>(breachType) << "\n";
+}
+
+// Send alert to email based on the breach type
+void TypewiseAlert::sendToEmail(BreachType breachType) {
+    if (breachType == BreachType::TOO_HIGH) {
+        std::cout << "To: a.b@c.com\nHi, the temperature is too high\n";
     }
 }
 
-void BatteryAlertSystem::checkAndAlert(AlertTarget alertTarget, const BatteryCharacter& batteryChar, double temperature) {
-    BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperature);
-    
+// Check and send alert based on the target, battery character, and temperature
+void TypewiseAlert::checkAndAlert(AlertTarget alertTarget, const BatteryCharacter& batteryChar, double temperatureInC) {
+    BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
     switch (alertTarget) {
         case AlertTarget::TO_CONTROLLER:
-            std::cout << "feed : " << static_cast<int>(breachType) << "\n";
+            sendToController(breachType);
             break;
         case AlertTarget::TO_EMAIL:
-            if (breachType == BreachType::TOO_HIGH) {
-                std::cout << "To: a.b@c.com\nHi, the temperature is too high\n";
-            }
+            sendToEmail(breachType);
             break;
     }
 }
