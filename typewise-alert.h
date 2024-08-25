a8072410971345e61b766868e48761c5265cd819
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-08-25 20:39:26 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,47 +1,43 @@
 #ifndef TYPEWISE_ALERT_H
 #define TYPEWISE_ALERT_H
 
-#include <string>
+enum class CoolingType {
+    PASSIVE_COOLING,
+    HI_ACTIVE_COOLING,
+    MED_ACTIVE_COOLING
+};
+
+enum class BreachType {
+    NORMAL,
+    TOO_LOW,
+    TOO_HIGH
+};
+
+enum class AlertTarget {
+    TO_CONTROLLER,
+    TO_EMAIL
+};
+
+struct BatteryCharacter {
+    CoolingType coolingType;
+    char brand[48];
+};
 
 class TypewiseAlert {
 public:
-    enum class CoolingType {
-        PASSIVE_COOLING,
-        HI_ACTIVE_COOLING,
-        MED_ACTIVE_COOLING
-    };
-
-    enum class BreachType {
-        NORMAL,
-        TOO_LOW,
-        TOO_HIGH
-    };
-
-    enum class AlertTarget {
-        TO_CONTROLLER,
-        TO_EMAIL
-    };
-
-    struct BatteryCharacter {
+    struct CoolingParameters {
         CoolingType coolingType;
-        std::string brand;
+        double lowerLimit;
+        double upperLimit;
     };
-
-    static BreachType assessTemperatureBreach(CoolingType coolingType, double temperatureInC);
-    static void checkAndAlert(AlertTarget alertTarget, const BatteryCharacter& batteryChar, double temperatureInC);
-    static BreachType evaluateBreach(double value, double lowerLimit, double upperLimit);
-
-private:
-    struct CoolingLimits {
-        CoolingType coolingType;
-        int lowerLimit;
-        int upperLimit;
-    };
-
-    static CoolingLimits findLimitsForCoolingType(CoolingType coolingType);
-    static const CoolingLimits* retrieveCoolingLimits();
-    static void alertController(BreachType breachType);
-    static void alertEmail(BreachType breachType);
+    
+    static const CoolingParameters* getCoolingParameters();
+    static CoolingParameters getParamsForCoolingType(CoolingType coolingType);
+    static BreachType evaluateBreach(double temperature, double lowerLimit, double upperLimit);
+    static BreachType classifyTemperature(double temperatureInC, CoolingType coolingType);
+    static void sendToController(BreachType breachType);
+    static void sendToEmail(BreachType breachType);
+    static void monitorAndAlert(AlertTarget alertTarget, const BatteryCharacter& batteryChar, double temperatureInC);
 };
 
 #endif // TYPEWISE_ALERT_H
