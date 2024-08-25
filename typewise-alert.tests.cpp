7804a4241fbb15e74fc2ec4e80af361f13bbb696
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-08-25 20:41:59 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -1,23 +1,32 @@
 #include "typewise-alert.h"
 #include <cassert>
+#include <cstdio>
 
+// Function to test temperature classification
 void testTemperatureClassification() {
     assert(TypewiseAlert::classifyTemperature(20, CoolingType::PASSIVE_COOLING) == BreachType::NORMAL);
     assert(TypewiseAlert::classifyTemperature(50, CoolingType::HI_ACTIVE_COOLING) == BreachType::TOO_HIGH);
     assert(TypewiseAlert::classifyTemperature(-5, CoolingType::MED_ACTIVE_COOLING) == BreachType::TOO_LOW);
 }
 
-void testAlertingMechanism() {
-    BatteryCharacter batteryChar = {CoolingType::HI_ACTIVE_COOLING, "BrandA"};
-    
-    TypewiseAlert::monitorAndAlert(AlertTarget::TO_CONTROLLER, batteryChar, 30);
-    TypewiseAlert::monitorAndAlert(AlertTarget::TO_EMAIL, batteryChar, 50);
+// Function to test the alert mechanism
+void testTypewiseAlert() {
+    BatteryCharacter batteryChar{CoolingType::MED_ACTIVE_COOLING, "BrandX"};
+
+    // Test alert for temperature that is too high
+    TypewiseAlert::monitorAndAlert(AlertTarget::TO_CONTROLLER, batteryChar, 45);
+    // Since 45 is above the MED_ACTIVE_COOLING limit, it should trigger a TO_CONTROLLER alert.
+
+    // Test alert for temperature within the normal range
+    TypewiseAlert::monitorAndAlert(AlertTarget::TO_EMAIL, batteryChar, 35);
+    // Since 35 is within the MED_ACTIVE_COOLING limit, it should not trigger an alert.
 }
 
 int main() {
+    // Run both tests
     testTemperatureClassification();
-    testAlertingMechanism();
-    
+    testTypewiseAlert();
+
     printf("All tests passed!\n");
     return 0;
 }
