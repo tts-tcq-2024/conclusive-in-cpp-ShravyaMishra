d7f899aafad162b4416fa9647c1c4db67ab393c1
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-08-25 21:03:23 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,19 +1,20 @@
-#pragma once
+#ifndef TYPEWISE_ALERT_H
+#define TYPEWISE_ALERT_H
 
 #include <string>
 
 class TypewiseAlert {
 public:
-    enum class BreachType {
-        NORMAL,
-        TOO_LOW,
-        TOO_HIGH
-    };
-
     enum class CoolingType {
         PASSIVE_COOLING,
         HI_ACTIVE_COOLING,
         MED_ACTIVE_COOLING
+    };
+
+    enum class BreachType {
+        NORMAL,
+        TOO_LOW,
+        TOO_HIGH
     };
 
     enum class AlertTarget {
@@ -26,19 +27,21 @@ public:
         std::string brand;
     };
 
-    static BreachType evaluateBreach(double temperature, double lowerLimit, double upperLimit);
-    static BreachType classifyTemperature(double temperatureInC, CoolingType coolingType);
-    static void sendToController(BreachType breachType);
-    static void sendToEmail(BreachType breachType);
-    static void monitorAndAlert(AlertTarget alertTarget, const BatteryCharacter& batteryChar, double temperatureInC);
-
-private:
     struct CoolingParameters {
         CoolingType coolingType;
         double lowerLimit;
         double upperLimit;
     };
 
+    static BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+    static void checkAndAlert(AlertTarget alertTarget, const BatteryCharacter& batteryChar, double temperatureInC);
+
+private:
+    static BreachType inferBreach(double value, double lowerLimit, double upperLimit);
     static const CoolingParameters* getCoolingParameters();
     static CoolingParameters getParamsForCoolingType(CoolingType coolingType);
+    static void sendToController(BreachType breachType);
+    static void sendToEmail(BreachType breachType);
 };
+
+#endif // TYPEWISE_ALERT_H
